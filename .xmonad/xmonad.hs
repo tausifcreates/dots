cab79771c581947ed9e3b226fb8158fbf71eada7
ino <tawsifriju@gmail.com>
2021-08-10 02:53:23 +0600

    new

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,6 +1,7 @@
 import XMonad
 import Data.Monoid
 import System.Exit
+import XMonad.Util.SpawnOnce
 import Graphics.X11.ExtraTypes.XF86
 import XMonad.Hooks.DynamicLog
 import XMonad.Layout.Gaps
@@ -8,7 +9,6 @@ import XMonad.Layout.Spacing
 import XMonad.Actions.CycleWS
 import qualified XMonad.StackSet as W
 import qualified Data.Map        as M
-import XMonad.Util.SpawnOnce
 
 -- The preferred terminal program, which is used in a binding below and by
 -- certain contrib modules.
@@ -120,16 +120,16 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     
     -- a basic CycleWS setup
 
-    , ((modm,               xK_w),  nextWS)
-    , ((modm,               xK_q),    prevWS)
-    , ((modm .|. shiftMask, xK_w),  shiftToNext)
-    , ((modm .|. shiftMask, xK_q),    shiftToPrev)
+    , ((modm,               xK_w),   nextWS)
+    , ((modm,               xK_q),   prevWS)
+    , ((modm .|. shiftMask, xK_w),   shiftToNext)
+    , ((modm .|. shiftMask, xK_q),   shiftToPrev)
 
 
     , ((modm,               xK_Right),  nextWS)
-    , ((modm,               xK_Left),    prevWS)
+    , ((modm,               xK_Left),   prevWS)
     , ((modm .|. shiftMask, xK_Right),  shiftToNext)
-    , ((modm .|. shiftMask, xK_Left),    shiftToPrev)
+    , ((modm .|. shiftMask, xK_Left),   shiftToPrev)
     -- Toggle the status bar gap
     -- Use this binding with avoidStruts from Hooks.ManageDocks.
     -- See also the statusBar function from Hooks.DynamicLog.
@@ -141,9 +141,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
 
     -- Restart xmonad
     , ((modm              , xK_r     ), spawn "xmonad --recompile; xmonad --restart")
-
-    -- Run xmessage with a summary of the default keybindings (useful for beginners)
-    , ((modm .|. shiftMask, xK_slash ), spawn ("echo \"" ++ help ++ "\" | xmessage -file -"))
+    
     ]
     ++
 
@@ -186,7 +184,7 @@ myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
 -- The available layouts.  Note that each layout is separated by |||,
 -- which denotes layout choice.
 --
-myLayout = spacingRaw False (Border 0 0 0 0) False (Border 6 6 6 6) True $ gaps [(U, 15), (R, 15), (D, 15), (L, 15)] $ tiled ||| Mirror tiled ||| Full
+myLayout = spacingRaw False (Border 0 0 0 0) False (Border 6 6 6 6) True $ gaps [(U, 4), (R, 4), (D, 4), (L, 4)] $ tiled ||| Mirror tiled ||| Full
   where
      -- default tiling algorithm partitions the screen into two panes
      tiled   = Tall nmaster delta ratio
@@ -299,61 +297,3 @@ defaults = def {
         logHook            = myLogHook,
         startupHook        = myStartupHook
     }
-
--- | Finally, a copy of the default bindings in simple textual tabular format.
-help :: String
-help = unlines ["The default modifier key is 'alt'. Default keybindings:",
-    "",
-    "-- launching and killing programs",
-    "mod-Shift-Enter  Launch alacritty",
-    "mod-p            Launch dmenu",
-    "mod-Shift-a      Close/kill the focused window",
-    "mod-Space        Rotate through the available layout algorithms",
-    "mod-Shift-Space  Reset the layouts on the current workSpace to default",
-    "mod-n            Resize/refresh viewed windows to the correct size",
-    "",
-    
-    "-- move focus up or down the window stack",
-    "mod-Tab        Move focus to the next window",
-    "mod-Shift-Tab  Move focus to the previous window",
-    "mod-j          Move focus to the next window",
-    "mod-k          Move focus to the previous window",
-    "mod-m          Move focus to the master window",
-    "",
-    
-    "-- modifying the window order",
-    "mod-Return   Swap the focused window and the master window",
-    "mod-Shift-j  Swap the focused window with the next window",
-    "mod-Shift-k  Swap the focused window with the previous window",
-    "",
-    
-    "-- resizing the master/slave ratio",
-    "mod-h  Shrink the master area",
-    "mod-l  Expand the master area",
-    "",
-    
-    "-- floating layer support",
-    "mod-t  Push window back into tiling; unfloat and re-tile it",
-    "",
-    
-    "-- increase or decrease number of windows in the master area",
-    "mod-comma  (mod-,)   Increment the number of windows in the master area",
-    "mod-period (mod-.)   Deincrement the number of windows in the master area",
-    "",
-    
-    "-- quit, or restart",
-    "mod-Shift-q  Quit xmonad",
-    "mod-q        Restart xmonad",
-    "mod-[1..9]   Switch to workSpace N",
-    "",
-    
-    "-- Workspaces & screens",
-    "mod-Shift-[1..9]   Move client to workspace N",
-    "mod-{w,e,r}        Switch to physical/Xinerama screens 1, 2, or 3",
-    "mod-Shift-{w,e,r}  Move client to screen 1, 2, or 3",
-    "",
-    
-    "-- Mouse bindings: default actions bound to mouse events",
-    "mod-button1  Set the window to floating mode and move by dragging",
-    "mod-button2  Raise the window to the top of the stack",
-    "mod-button3  Set the window to floating mode and resize by dragging"]
